f988e04cc25902a53faa4e9f6ece0096fdfe3799
Matteo Marinelli <mmarinel@e3r10p4.42roma.it>
2022-08-20 19:34:44 +0200

    ex04: wip

--- ex04/my_sed.cpp
+++ ex04/my_sed.cpp
@@ -6,16 +6,22 @@
 /*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 15:39:06 by mmarinel          #+#    #+#             */
-/*   Updated: 2022/08/20 19:00:46 by mmarinel         ###   ########.fr       */
+/*   Updated: 2022/08/20 19:33:58 by mmarinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ex04.hpp"
 
-static void	substitute_all(std::string filename, std::string to_find, std::string replacement,
-				std::ifstream &myFile, std::ofstream &output);
 static void	open_fail( void );
 static void	read_write_fail(std::ifstream &myFile, std::ofstream &output);
+static void	substitute_all(std::string to_find, std::string replacement,
+				std::ifstream &myFile, std::ofstream &output);
+static void	shift_buffer(std::string &buffer, std::string to_find,
+				std::ofstream &output);
+static void replace_line(size_t new_pos,
+				std::string &buffer, std::string replacement,
+				std::ofstream &output);
+static bool	find_success(size_t index);
 //* end of static declarations
 
 /**
@@ -27,29 +33,29 @@ static void	read_write_fail(std::ifstream &myFile, std::ofstream &output);
  * @return true 
  * @return false 
  */
-// bool	my_sed(std::string filename, std::string to_find, std::string replacement)
-// {
-// 	std::ifstream	myFile (filename);
-// 	std::ofstream	output (std::string(filename) + std::string(".replace"));
+bool	my_sed(std::string filename, std::string to_find, std::string replacement)
+{
+	std::ifstream	myFile (filename);
+	std::ofstream	output (std::string(filename) + std::string(".replace"));
 
-// 	if (false == myFile.is_open() || false == output.is_open())
-// 	{
-// 		open_fail();
-// 		return (false);
-// 	}
-// 	else
-// 	{
-// 		substitute_all(myFile, output);
-// 		if (myFile.bad())
-// 		{
-// 			read_write_fail(to_find, replacement, myFile, output);
-// 			return (false);
-// 		}
-// 	}
-// 	return (true);
-// }
+	if (false == myFile.is_open() || false == output.is_open())
+	{
+		open_fail();
+		return (false);
+	}
+	else
+	{
+		substitute_all(to_find, replacement, myFile, output);
+		if (myFile.bad())
+		{
+			read_write_fail(myFile, output);
+			return (false);
+		}
+	}
+	return (true);
+}
 
-static void	substitute_all(std::string filename, std::string to_find, std::string replacement,
+static void	substitute_all(std::string to_find, std::string replacement,
 				std::ifstream &myFile, std::ofstream &output)
 {
 	std::string	buffer;
@@ -61,43 +67,50 @@ static void	substitute_all(std::string filename, std::string to_find, std::strin
 	while (myFile.read(line, to_find.length()))
 	{
 		buffer += std::string(line);
-		new_pos = buffer.find(buffer);
-		if (new_pos == std::string::npos)
+		new_pos = buffer.find(to_find);
+		if (false == find_success(new_pos))
 		{
-			new_pos = buffer.find(to_find[0]);
-			if (new_pos == std::string::npos)
-			{
-				output << buffer;
-				buffer.erase(0);
-			}
-			else
-			{
-				output << buffer.substr(0, new_pos);
-				buffer = buffer.substr(new_pos);
-			}
+			shift_buffer(buffer, to_find, output);
 		}
 		else
 		{
-			if (new_pos == 0)
-				output << replacement;
-			else
-				output << buffer.substr(0, new_pos) << replacement;
-			new_pos = new_pos + replacement.length();
-			if (new_pos > buffer.length() - 1)
-				buffer.erase(0);
-			else
-				buffer = buffer.substr(new_pos);
+			replace_line(new_pos, buffer, replacement, output);
 		}
 	}
 
 }
 
-static void	load_file(std::ifstream &myFile, std::string &buffer)
+static void	shift_buffer(std::string &buffer, std::string to_find,
+				std::ofstream &output)
 {
-	std::string	next_line;
+	size_t		new_pos;
 
-	while (getline(myFile, next_line))
-		buffer += next_line;
+	new_pos = buffer.find(to_find[0]);
+	if (false == find_success(new_pos))
+	{
+		output << buffer;
+		buffer.erase(0);
+	}
+	else
+	{
+		output << buffer.substr(0, new_pos);
+		buffer = buffer.substr(new_pos);
+	}
+}
+
+static void replace_line(size_t new_pos,
+				std::string &buffer, std::string replacement,
+				std::ofstream &output)
+{
+	if (new_pos == 0)
+		output << replacement;
+	else
+		output << buffer.substr(0, new_pos) << replacement;
+	new_pos = new_pos + replacement.length();
+	if (new_pos > buffer.length() - 1)
+		buffer.erase(0);
+	else
+		buffer = buffer.substr(new_pos);
 }
 
 static void	open_fail( void )
@@ -112,4 +125,9 @@ static void read_write_fail(std::ifstream &myFile, std::ofstream &output)
 	output.close();
 	//* we don't write to cerr because 'General rules' say write to cout unless explicitly told otherwise'
 	std::cout << RED << "Error: write/read operation failed" << RESET << std::endl;
+}
+
+static bool	find_success(size_t index)
+{
+	return (index != std::string::npos);
 }
